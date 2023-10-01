e2fd554b269430949307be1200a9f0217b6bf1e1
Otstavnov A.M <alexotst58@yandex.ru>
2023-10-01 16:17:33 +0300

    Добавлено избранное, отдельные страницы для книги

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,50 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Book } from "./Book";
-import { IBook } from "./models";
-import axios from "axios";
+import BookList from "./pages/BookList";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const keyApi1 = "AIzaSyAWXUr33PRKRXXG4yFt0fvgALVcgXABHn8";
-  const keyApi2 = "AIzaSyDPG9XRUg6u_g6zzkGibtZ3n6wrxhzTKbU";
-  const [offset, setOffset] = useState(0);
-  const [loadedBooks, setLoadedBooks] = useState<IBook[]>([]);
-  const [hasMoreBooks, setHasMoreBooks] = useState(true);
-  const [noBooksFound, setNoBooksFound] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [corSearchValue, setCorSearchValue] = useState("");
 
-  const searchBook = (evt: React.KeyboardEvent<HTMLInputElement>) => {
-    if (evt.key === "Enter" && search != "") {
-      setOffset(0);
-      setHasMoreBooks(true);
-      setNoBooksFound(false);
-      setLoadedBooks([]);
-      try {
-        axios
-          .get(
-            `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&key=${keyApi2}&startIndex=${offset}&maxResults=40`,
-          )
-          .then((response) => {
-            setLoadedBooks(response.data.items);
-            console.log(response.data.totalItems);
-            console.log(response.data.items);
-            if (response.data.totalItems <= offset + 40) {
-              setHasMoreBooks(false);
-            }
-            if (response.data.totalItems === 0) {
-              setNoBooksFound(true);
-            }
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    console.log(noBooksFound);
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
   };
-
-  const loadMoreBook = () => {
-    try {
-      axios
-        .get(
-          `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&key=${keyApi2}&startIndex=${offset}&maxResults=40`,
-        )
-        .then((response) => {
-          const newBooks = response.data.items.filter(
-            (book: IBook) =>
-              !loadedBooks.some((loadedBook) => loadedBook.id === book.id),
-          );
-          setLoadedBooks((prevBooks) => [...prevBooks, ...newBooks]);
-          setOffset((prevOffset) => prevOffset + 40);
-          console.log(response.data.items);
-          if (response.data.totalItems <= offset + 40) {
-            setHasMoreBooks(false);
-          }
-        });
-    } catch (error) {
-      console.error(error);
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setCorSearchValue(searchValue);
     }
   };
-
-  const handleScroll = (e: any) => {
-    if (
-      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
-        e.target.documentElement.scrollHeight &&
-      hasMoreBooks
-    ) {
-      loadMoreBook();
-    }
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate(`/favourites`);
   };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [loadedBooks, hasMoreBooks]);
 
   return (
     <>
       <div className="navbar">
         <ul>
-          <li>{/* <img src={"./books.png"} alt="эмблема" /> */}</li>
           <li>
             <div className="title"> Книжная лавка</div>
           </li>
           <li>
+            <div className="title" onClick={onClick}>
+              {" "}
+              Избранное
+            </div>
+          </li>
+          <li>
             <input
               type="text"
-              placeholder="Введите название или автора книги"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyUp={searchBook}
+              placeholder="Введите название или автора книги "
+              value={searchValue}
+              onChange={handleSearchChange}
+              onKeyUp={handleKeyPress}
             />
           </li>
         </ul>
       </div>
-      <div className="container">
-        {noBooksFound ? (
-          <div className="noBooksFound">Книги не найдены</div>
-        ) : (
-          loadedBooks.map((book) => <Book book={book} key={book.id} />)
-        )}
-      </div>
+      <BookList searchValue={corSearchValue} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import BookCard from "./pages/BookCard.tsx";
+import BookList from "./pages/BookList.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Favourites from "./pages/Favourites.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/book",
+    element: <BookCard />,
+  },
+  {
+    path: "/favourites",
+    element: <Favourites />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
